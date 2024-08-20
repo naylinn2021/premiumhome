7d47e34b5b5c455442627fb72973a099857258d8
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-08-20 23:01:36 +0630

    Update jsonconnectfree.js

--- jsonconnectfree.js
+++ jsonconnectfree.js
@@ -10,7 +10,7 @@ function(result) {
 });
 
 // jsonconnect national sport
-$.getJSON('acarafavorit.json', 
+$.getJSON('/acarafavorit.json', 
 function(result) {
   let nationalsp = result.nationalsp;
   //console.log(event);
