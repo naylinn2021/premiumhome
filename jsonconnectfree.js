37a3ce22a09e79c86b09b414af87c675c7f5dc0f
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-08-20 23:03:14 +0630

    Update jsonconnectfree.js

--- jsonconnectfree.js
+++ jsonconnectfree.js
@@ -10,7 +10,7 @@ function(result) {
 });
 
 // jsonconnect national sport
-$.getJSON('/acarafavorit.json', 
+$.getJSON('https://blacktvreborn.my.id/database/liveevent/acarafavorit.json', 
 function(result) {
   let nationalsp = result.nationalsp;
   //console.log(event);
