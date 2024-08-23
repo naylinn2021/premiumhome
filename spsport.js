10d1e9f5b7c32006e9f6aefb730afc6f80d58127
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-08-23 20:14:17 +0630

    Update

---
+++ spsport.js
@@ -0,0 +1,12 @@
+function super1() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://live.95tv.live/channel/24/playlist.m3u8|referer=http://live.95tv/|origin=http://live.95tv/&User-Agent=Mozilla/5.0#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function super2() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://live.95tv.live/channel/10/playlist.m3u8|referer=http://live.95tv/|origin=http://live.95tv/&User-Agent=Mozilla/5.0#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function super3() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn2.aluku91.foundation/z-supersportpremierleague.m3u8|referer=https://365streamtv.live/|origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function super4() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn4.aluku91.foundation/linea6.m3u8|referer=https://365streamtv.live/|origin=https://365streamtv.live/  &User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
