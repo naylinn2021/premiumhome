8b7adab91587e1a566128f28075773f4d2092bb9
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-08-23 20:16:13 +0630

    Update

---
+++ star sport.js
@@ -0,0 +1,7 @@
+function star1hd() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn2.aluku91.foundation/z-starsportselect1.m3u8|referer=https://365streamtv.live/|origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function star2hd() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn2.aluku91.foundation/z-starsportselect2.m3u8|referer=https://365streamtv.live/|origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+
