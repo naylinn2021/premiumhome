911377f580254365966cd4142984771880c8cd03
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-08-28 00:55:49 +0630

    Update spsport.js

--- spsport.js
+++ spsport.js
@@ -10,3 +10,7 @@ function super3() {
 function super4() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://cdn2.aluku91.foundation/epl-optussport6.m3u8|referer=https://365streamtv.live/|origin=https://365streamtv.live/  &User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
+
+function super5() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn2.aluku91.foundation/epl-optussport2.m3u8|referer=https://365streamtv.live/|origin=https://365streamtv.live/  &User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
