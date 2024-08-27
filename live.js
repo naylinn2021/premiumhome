d5ab447bd9c15a4a9a04b7d938d5d5870a59ddad
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-08-27 23:22:18 +0630

    Update live.js

--- live.js
+++ live.js
@@ -1,5 +1,5 @@
 function live1() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://pull.niues.live/live/stream-9912052_lhd.m3u8?auth_key=1724780902-0-0-d46926131ddb8624072e3024d0e47f0b|referer=https://m.socoliveb.me/origin=https://m.socoliveb.me/&user-agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://pull.niues.live/live/stream-9912052_lsd.m3u8?auth_key=1724780902-0-0-abacdc1a98e4e771b2d1dd6220eccb9e|referer=https://m.socoliveb.me/origin=https://m.socoliveb.me/&user-agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function live2() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://live.95tv.live/channel/27/playlist.m3u8|referer=https://95tv.live/origin=https://95tv.live/#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
