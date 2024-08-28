2b1f349febadba9d1129cc373defb63dfa7994ad
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-08-29 02:27:50 +0630

    Update sport.js

--- sport.js
+++ sport.js
@@ -41,7 +41,7 @@ document.getElementById("objek").innerHTML = '<iframe src="intent://live-tv.yarn
 };
 function sporttv1() {
 
-document.getElementById("objek").innerHTML = '<iframe src="intent://cdn2.aluku91.foundation/pt-sporttv1.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+document.getElementById("objek").innerHTML = '<iframe src="intent://cdn2.aluku91.foundation/pt-sporttv1.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/|&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function sporttv2() {
 
