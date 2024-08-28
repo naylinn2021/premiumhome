db9ab38a99ab5dd073e88c32016d4d4e82b098c3
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-08-29 03:29:39 +0630

    Update portugal.js

--- portugal.js
+++ portugal.js
@@ -1,14 +1,15 @@
-function sptv1() {
+function psport1() {
 document.getElementById("objek").innerHTML = '<iframe src="intent://cdn2.aluku91.foundation/pt-sporttv1.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 
-function sptv2() {
+function psport2() {
 document.getElementById("objek").innerHTML = '<iframe src="intent://rr3-ic3d-ndjcs.huaweicdncloud.com/dooball2you/sporttv2/playlist.m3u8|referer=https://dooball2you.com/origin=https://dooball2you.com/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 
-function sptv3() {
+function psport3() {
 document.getElementById("objek").innerHTML = '<iframe src="intent://cdn4.aluku91.foundation/pt-sporttv3.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 
-function sptv4() {
+function psport4() {
 document.getElementById("objek").innerHTML = '<iframe src="intent://rr3-ic3d-ndjcs.huaweicdncloud.com/dooball2you/sporttv4/playlist.m3u8|referer=https://dooball2you.com/origin=https://dooball2you.com/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
