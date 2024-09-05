680863bccf062bdcdf1934a90c86c965e17bc895
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-09-05 13:27:48 +0630

    Update jsonconnectfree.js

--- jsonconnectfree.js
+++ jsonconnectfree.js
@@ -97,6 +97,17 @@ function(result) {
   });
 });
 
+// jsonconnect MOVIE
+$.getJSON('movie.json', 
+function(result) {
+  let movie = result.movie;
+  //console.log(movie);
+
+  $.each(movie, function(i, result) {
+    $('#movie').append('<div onclick="' + result.url + '()" class="movie_item"><img src="' + result.poster + '" class="movie_image" /><h3><span>' + result.tanggal + '</span></h3><h4><span>' + result.jam + '</span></h4></div>');
+  });
+});
+
 // jsonconnect sportlist
 $.getJSON('https://blacktvreborn.my.id/database/liveevent/sportlist.json', 
 function(result) {
