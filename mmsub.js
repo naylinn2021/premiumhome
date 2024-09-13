b85684c87614875226ed8e53088e40680f6b6e92
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-09-13 22:31:53 +0630

    Update

---
+++ mmsub.js
@@ -0,0 +1,117 @@
+function six_Hours_Away() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/662a1a872dbc659b47b24a77#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function City_Hunter() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/662a938b2dbc659b47b24a7c#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function The_Fall_Guy() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/664dcdd3a57ee894ff0cc2a2#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Boy_Kills_World() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/665057bb4980d4f686ce6198#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function The_Roundup_Punishment() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66866febb8d4c322c17afeb7#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Rathnam() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/6652c2354980d4f686ce61ae#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Oru_Nodi() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/665b0385b753c45946f68014#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Down_the_Rabbit_Hole() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/664a020aa57ee894ff0cc28a#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function The_Garfield() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/668fc02db8d4c322c17afeda#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Tarot() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/665c5bb8b753c45946f68021#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Darkness_of_Man() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/664f51414980d4f686ce618e#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Mother_of_the_Bride() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/664b5a5da57ee894ff0cc29b#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Kingdom_of_the_Planet_of_the_Apes() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/668d06f4b8d4c322c17afed0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Krishnamma() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66486e89abfd229e3e9e1d80#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Chasing_Raine() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/664a0714a57ee894ff0cc28c#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Silence_of_the_Prey() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66548666b3f10ffbc4d13407#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function The_Strangers_Chapter_1() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66687b3c1e3a972e7535163a#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Tipline_Mysteries_Dial_1_for_Murder() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66cc8305c3c52d177aba12e4#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Atlas() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/665344a3b3f10ffbc4d133fd#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function The_Plot() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66b07fd3be36e6339190d871#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Die_Hart_2() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/665ef947286852bae4b59dae#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Under_Paris() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/6666ab55ea1f477bad5d596a#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Wonderland() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66a3aaa2be36e6339190d82e#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Satyabhama() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/668675dfb8d4c322c17afeb9#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Golam() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66bf46bec3c52d177aba126c#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Drive() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66bb5b14c3c52d177aba1249#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Tarot() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/669cac7fbe36e6339190d81b#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function The_Bikeriders_2023() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/669bbac7be36e6339190d813#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Despicable_Me_4() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66b23179be36e6339190d881#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function Hijack_1971() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66ac7292be36e6339190d856#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function A_Quiet_Place_Day_One() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://rainbowchannel.rmcmm.org/dl/66a50d4ebe36e6339190d835#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function child1() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ko.htay.workers.dev/0:/-Books%26Movies_Htay/Movies%20List/Movies%20By%20Name/%5B%20C%20%5D/(CM)%20Child%E2%80%99s%20Play%20(1988)%20720P.mp4#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function child2() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ko.htay.workers.dev/0:/-Books%26Movies_Htay/Movies%20List/Movies%20By%20Name/%5B%20C%20%5D/(CM)%20Child%E2%80%99s%20Play%202%20(1990)%20720P.mp4#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function child3() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ko.htay.workers.dev/0:/-Books%26Movies_Htay/Movies%20List/Movies%20By%20Name/%5B%20C%20%5D/(CM)%20Child%E2%80%99s%20Play%203%20(1991)%20720P.mp4#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function child4() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ko.htay.workers.dev/0:/-Books%26Movies_Htay/Movies%20List/Movies%20By%20Name/%5B%20B%20%5D/(CM)%20Bride%20Of%20Chucky%20[1998]%20(MM%20Sub).mp4#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=𝑴𝒚𝒕𝒗 𝒑𝒓𝒆𝒎𝒊𝒖𝒎;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function child5() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ko.htay.workers.dev/0:/-Books%26Movies_Htay/Movies%20List/Movies%20By%20Name/%5B%20S%20%5D/%20%20(CM)%20Seed%20of%20Chucky%20(2004).mp4#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function child6() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ko.htay.workers.dev/0:/-Books%26Movies_Htay/Movies%20List/Movies%20By%20Name/%5B%20C%20%5D/(CM)%20Curse%20Of%20Chucky%20[2013]%20(MM%20Sub).mp4#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=𝑴𝒚𝒕𝒗 𝒑𝒓𝒆𝒎𝒊𝒖𝒎;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function child7() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ko.htay.workers.dev/0:/-Books%26Movies_Htay/Movies%20List/Movies%20By%20Name/%5B%20C%20%5D/(CM)%20Cult%20Of%20Chucky%20[2017]%20(MM%20Sub).mp4#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=𝑴𝒚𝒕𝒗 𝒑𝒓𝒆𝒎𝒊𝒖𝒎;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
+function child8() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ko.htay.workers.dev/0:/-Books%26Movies_Htay/Movies%20List/Movies%20By%20Name/%5B%20C%20%5D/%20Childs-Play-2019-HD-GC.mp4#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
