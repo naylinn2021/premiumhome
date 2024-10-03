751108b8d0541c09a927c3ddd80aaaf790883824
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-10-04 01:28:35 +0630

    Update live.js

--- live.js
+++ live.js
@@ -1,5 +1,5 @@
 function live1() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://live.95tv.live/channel/25/playlist.m3u8|referer=https://95tv.live/origin=https://95tv.live/&user-agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://pull.niues.live/live/stream-499765_lsd.m3u8?auth_key=1727985385-0-0-dd28e0229f81e8369835026296e24b75|referer=https://m.socolivep.com/origin=https://m.socolivep.com/&user-agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function live2() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://pull.niues.live/live/stream-9912041_lsd.m3u8?auth_key=1727618245-0-0-f62b9142df5f35d2f89acee849ca5418|referer=https://m.socolivez.app/origin=https://m.socolivez.app/&user-agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
