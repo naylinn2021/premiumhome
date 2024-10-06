d98158a338a734bf6db8cc345882be63b1b902cd
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-10-07 01:51:16 +0630

    Update

---
+++ movie2.js
@@ -0,0 +1,172 @@
+{
+  "sports": [    
+    {
+      "channelName": "HBO",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/hh/hbo_asia.png",
+      "mpdUrl": "https://cnt1-streamer9.cdn.3bbtv.com:8443/3bb/live/103/103.mpd",
+      "licenseKey": "9c5735afb4fd402580360aed8364469c:128d55d9ac2b47ad85f86b5d08320179",
+      "m3u8Url": "",
+      "webUrl": ""     
+    },
+    {
+      "channelName": "HBO Signature",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/hh/hbo_signature_asia.png",
+      "mpdUrl": "https://tglmp02.akamaized.net/out/v1/b37a81eb3585471cb9c639c78edcdefa/manifest.mpd",
+      "licenseKey": "90480f571f5d410891d7add7103c1e84:b64f887d2480bcaea992c2f0826449a9",
+      "m3u8Url": "",
+      "webUrl": ""    
+    },
+    {
+      "channelName": "HBO Hits",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/hh/hbo_hits.png",
+      "mpdUrl": "https://tglmp02.akamaized.net/out/v1/0fdcb3b643af40ceb3a5b7efa39f7b72/manifest.mpd",
+      "licenseKey": "473fabdc05ac4d9b94fd34f33f73394f:1072b1e61206c3859607984f872e549c",
+      "m3u8Url": "",
+      "webUrl": ""    
+    },
+    {
+      "channelName": "HBO FAMILY",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/hh/hbo_family_asia.png",
+      "mpdUrl": "https://tglmp03.akamaized.net/out/v1/d15476c2807d4380b0cc375a7d861752/manifest.mpd",
+      "licenseKey": "363b551913124d6b80074ca295b25129:a6c6efcab0076d572e5732fb6451d19c",
+      "m3u8Url": "",
+      "webUrl": ""   
+    },
+    {
+      "channelName": "CINEMAX",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/cc/cinemax-asia-us.png",
+      "mpdUrl": "https://tglmp04.akamaized.net/out/v1/0f439c1d8a1a47fca9ae1fa92144306e/manifest.mpd",
+      "licenseKey": "32bc2ee6869144b88bee3babf202f88e:50d3b9ec49e1570621d45822923607f7",
+      "m3u8Url": "",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "AXN HD",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/aa/axn_east_asia.png",
+      "mpdUrl": "https://atemecdnbalancer-voe.sysln.id/live/eds/AXNHD/mpd/AXNHD.mpd",
+      "licenseKey": "b7b00124894d4f1f8b91fba79160b896:bb236ab2b198c2ffd722817bfa74cd08",
+      "m3u8Url": "",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "Warner TV",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/ww/warner-tv-us.png",
+      "mpdUrl": "https://m7rovideolive.solocoo.tv/blueskylive2dash/bluetntro/Manifest.mpd",
+      "licenseKey": "cba822d983af4339901003e4f741a612:e0d2b9b74ef13dad35068a8b8ac9b3cf",
+      "m3u8Url": "",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "RockAction",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/rr/rock-action-sg.png",
+      "mpdUrl": "https://aqfadtv.xyz/live/mewatch/rockaction/index.mpd",
+      "licenseKey": "2341c2ecd3cf4865881bb0fa1287de8f:ad45202e64f66ef36b3f60fac93cc47f",
+      "m3u8Url": "",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "The Movie Club",
+      "channelLogo": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSl4UK0q5Be44pwSqSCwNhrdhlfKoj1o1xC_ksspUhn9XvWnTQz",
+      "mpdUrl": "",
+      "licenseKey": "",
+      "m3u8Url": "https://jmp2.uk/SamsungTVPlus/INBD21000016Y.m3u8",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "Rakuten Tv",
+      "channelLogo": "https://help.rakuten.tv/hc/theming_assets/01HZM1WQ46WRF8MSECBCH4YY2Z",
+      "mpdUrl": "",
+      "licenseKey": "",
+      "m3u8Url": "https://jmp2.uk/SamsungTVPlus/ESAJ45000181E.m3u8",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "Movie Sphere",
+      "channelLogo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO-4Yvi6v7j0pK11ZboA_m-C4lVCwSxMr_fdRT-4NGNiMte_1q",
+      "mpdUrl": "",
+      "licenseKey": "",
+      "m3u8Url": "https://amg01264-vidaausainc-moviesperebr-vidaa-zsk5f.amagi.tv/playlist/amg01264-vidaausainc-moviesperebr-vidaa/playlist.m3u8",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "Animax",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/aa/animax-jp.png",
+      "mpdUrl": "https://tglmp01.akamaized.net/out/v1/de55fad9216e4fe7ad8d2eed456ba1ec/manifest.mpd",
+      "licenseKey": "edf1a715de9748638dd2fad75a419af2:2f5a3199b26e9b693ae881af7ff864cf",
+      "m3u8Url": "",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "MONOPLUS",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/mm/mono_29_th.png",
+      "mpdUrl": "",
+      "licenseKey": "",
+      "m3u8Url": "https://streaming.monomax.me/Mono29LiveStream/ngrp:myStream_all/playlist.m3u8",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "TVN MOVIES",
+      "channelLogo": "https://images.indihometv.com/logo_tvnmovies_ver1.png",
+      "mpdUrl": "https://cdn08jtedge.indihometv.com/dassdvr/133/tvnmovies/manifest.mpd",
+      "licenseKey": "69646b755f3130303030303030303030:e4a2359b05563399f1d9adfce641724a",
+      "m3u8Url": "",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "GALAXY PREMIUM",
+      "channelLogo": "https://images.indihometv.com/logo_galaxypremium_small.png",
+      "mpdUrl": "https://cdn08jtedge.indihometv.com/dassdvr/133/galaxypremium/manifest.mpd",
+      "licenseKey": "69646b755f3130303030303030303030:e4a2359b05563399f1d9adfce641724a",
+      "m3u8Url": "",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "GALAXY",
+      "channelLogo": "https://images.indihometv.com/logo_galaxy_small.png",
+      "mpdUrl": "https://cdn08jtedge.indihometv.com/dassdvr/133/galaxy/manifest.mpd",
+      "licenseKey": "69646b755f3130303030303030303030:e4a2359b05563399f1d9adfce641724a",
+      "m3u8Url": "",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "CELESTIAL MOVIE",
+      "channelLogo": "https://images.indihometv.com/logo_celestialmovie_small.png",
+      "mpdUrl": "https://cdn08jtedge.indihometv.com/dassdvr/130/celestialmovie/manifest.mpd",
+      "licenseKey": "69646b755f3130303030303030303030:e4a2359b05563399f1d9adfce641724a",
+      "m3u8Url": "",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "MY CINEMA",
+      "channelLogo": "https://images.indihometv.com/logo_mycinema_ver1.png",
+      "mpdUrl": "https://cdn08jtedge.indihometv.com/dassdvr/133/mycinema/manifest.mpd",
+      "licenseKey": "69646b755f3130303030303030303030:e4a2359b05563399f1d9adfce641724a",
+      "m3u8Url": "",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "CINEMAWORLD",
+      "channelLogo": "https://images.indihometv.com/logo_cinemaworldnew_small.png",
+      "mpdUrl": "https://cdn08jtedge.indihometv.com/dassdvr/134/cinemaworld/manifest.mpd",
+      "licenseKey": "69646b755f3130303030303030303030:e4a2359b05563399f1d9adfce641724a",
+      "m3u8Url": "",
+      "webUrl": ""      
+    },
+    {
+      "channelName": "PlayBoy",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/pp/playboy_tv_us.png",
+      "mpdUrl": "https://cdnlb.tvplayhome.lt/live/eds/Playboy/GO3_LIVE_DASH/Playboy.mpd",
+      "licenseKey": "1ab63d6bb145481dad73df7ddb3505b3:b09ce3a46e43ac6cedb32eb5fa554779",
+      "m3u8Url": "",
+      "webUrl": ""     
+    },
+    {
+      "channelName": "Blue Hustler TV",
+      "channelLogo": "https://www.lyngsat.com/logo/tv/bb/blue_hustler_eu.png",
+      "mpdUrl": "https://cdnlb.tvplayhome.lt/live/eds/Blue_Hustler_TV/GO3_LIVE_DASH/Blue_Hustler_TV.mpd",
+      "licenseKey": "4a37e0f816f54bd19119e2a8e7fafb04:7e9e36ed4de7a3608e7a7aa8293f9db8",
+      "m3u8Url": "",
+      "webUrl": ""     
+    }          
+  ]
+}
