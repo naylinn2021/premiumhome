601288425bebdb938a70de8811338a1b998d1261
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-10-10 00:33:50 +0630

    Update Korea.js

--- Korea.js
+++ Korea.js
@@ -17,3 +17,6 @@ document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/pl
 function tvn2() {
 document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1007#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
+function mbn() {
+document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1008#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
