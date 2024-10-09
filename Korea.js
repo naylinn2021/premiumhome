90ac68c9ef60c502a812ed6b800e163fb3654dc6
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-10-09 22:59:23 +0630

    Update

---
+++ Korea.js
@@ -0,0 +1,3 @@
+function sbsgolf() {
+document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1001#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
