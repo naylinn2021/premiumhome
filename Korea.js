6108ef86b1c3aa9793963debdf42f6ace9dacbac
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-10-10 11:21:25 +0630

    Update Korea.js

--- Korea.js
+++ Korea.js
@@ -4,6 +4,9 @@ document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/pl
 function jtbcgolf() {
 document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1002#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
+function kworld() {
+document.getElementById("objek").innerHTML = '<iframe src="intent://kbsworld-ott.akamaized.net:443/hls/live/2002341/kbsworld/01.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+};
 function kbs1() {
 document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1004#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
