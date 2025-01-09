a1440f1402ea1690a81052dc5b5afc5cc9b4ceeb
mmgold <77553706+naylinn2021@users.noreply.github.com>
2025-01-10 00:03:06 +0630

    Update Korea.js

--- Korea.js
+++ Korea.js
@@ -1,31 +1,31 @@
 function sbsgolf() {
-document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1001#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+document.getElementById("objek").innerHTML = '<iframe src="intent://365tvda.com/plugin/livetv/pc/pc_view.php?co=01&ch=1001#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function jtbcgolf() {
-document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1002#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+document.getElementById("objek").innerHTML = '<iframe src="intent://365tvda.com/plugin/livetv/pc/pc_view.php?co=01&ch=1002#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function kworld() {
 document.getElementById("objek").innerHTML = '<iframe src="intent://kbsworld-ott.akamaized.net:443/hls/live/2002341/kbsworld/01.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function kbs1() {
-document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1004#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+document.getElementById("objek").innerHTML = '<iframe src="intent://365tvda.com/plugin/livetv/pc/pc_view.php?co=01&ch=1004#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function kbs2() {
-document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1005#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+document.getElementById("objek").innerHTML = '<iframe src="intent://365tvda.com/plugin/livetv/pc/pc_view.php?co=01&ch=1005#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function sbs() {
-document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1006#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+document.getElementById("objek").innerHTML = '<iframe src="intent://365tvda.com/plugin/livetv/pc/pc_view.php?co=01&ch=1006#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 
 function tvn2() {
-document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1007#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+document.getElementById("objek").innerHTML = '<iframe src="intent://365tvda.com/plugin/livetv/pc/pc_view.php?co=01&ch=1007#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function mbn() {
-document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1008#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+document.getElementById("objek").innerHTML = '<iframe src="intent://365tvda.com/plugin/livetv/pc/pc_view.php?co=01&ch=1008#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 
 function jtbc() {
-document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1009#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+document.getElementById("objek").innerHTML = '<iframe src="intent://365tvda.com/plugin/livetv/pc/pc_view.php?co=01&ch=1009#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function kdr() {
 document.getElementById("objek").innerHTML = '<iframe src="intent://livetv.work/play/kbs-n91.php#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
@@ -37,8 +37,8 @@ function kbsjoy() {
 document.getElementById("objek").innerHTML = '<iframe src="intent://livetv.work/play/kbs-n92.php#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function ocn() {
-document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1014#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+document.getElementById("objek").innerHTML = '<iframe src="intent://365tvda.com/plugin/livetv/pc/pc_view.php?co=01&ch=1014#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function pink() {
-document.getElementById("objek").innerHTML = '<iframe src="intent://ogtv1.com/plugin/livetv/pc/pc_view.php?co=01&ch=1013#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+document.getElementById("objek").innerHTML = '<iframe src="intent://365tvda.com/plugin/livetv/mobile/mobile_view.php?title=%ED%95%91%ED%81%AC%ED%95%98%EC%9A%B0%EC%8A%A4&co=01&ch=1013#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
