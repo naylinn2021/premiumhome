bc9bec82c75aef13a691d9c213f8b8bb1913dff6
mmgold <77553706+naylinn2021@users.noreply.github.com>
2025-09-19 15:35:15 +0630

    Create main/jsonconnect2.js

---
+++ main/jsonconnect2.js
@@ -0,0 +1,12 @@
+/* =========================
+   CONFIG (device + login info)
+========================= */
+const CONFIG = {
+  devices: {
+    "001788264119": { username: "admin1", password: "1111" },
+    "LID-IXYS4KHL": { username: "aaaa", password: "2222" },
+    "LID-9DKNNZL9": { username: "aung", password: "1221"},
+    "LID-MBH7YFPY": { username: "ZMT", password: "1234" }
+  },
+  iptvUrl: "https://your-iptv-link-here.com/"
+};
