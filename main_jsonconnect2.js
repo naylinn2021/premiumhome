cddd716981c7f72ab4e98d005a187f1298f71603
mmgold <77553706+naylinn2021@users.noreply.github.com>
2025-09-19 16:08:22 +0630

    Update main_jsonconnect2.js

--- main_jsonconnect2.js
+++ main_jsonconnect2.js
@@ -8,5 +8,5 @@ const CONFIG = {
     "LID-9DKNNZL9": { username: "aung", password: "1221"},
     "LID-MBH7YFPY": { username: "ZMT", password: "1234" }
   },
-  iptvUrl: "https://your-iptv-link-here.com/"
+  iptvUrl: "home.html"
 };
