a0008a9e25089883ba9f2841c62dabe5bde3ee8f
mmgold <77553706+naylinn2021@users.noreply.github.com>
2025-09-19 16:09:09 +0630

    Update main_jsonconnect2.js

--- main_jsonconnect2.js
+++ main_jsonconnect2.js
@@ -3,7 +3,7 @@
 ========================= */
 const CONFIG = {
   devices: {
-    "001788264119": { username: "admin1", password: "1111" },
+    "LID-TDCJSW7R": { username: "admin", password: "1111" },
     "LID-IXYS4KHL": { username: "aaaa", password: "2222" },
     "LID-9DKNNZL9": { username: "aung", password: "1221"},
     "LID-MBH7YFPY": { username: "ZMT", password: "1234" }
