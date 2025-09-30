28165f773c1a95cf7bba34f502b2433ffae69118
mmgold <77553706+naylinn2021@users.noreply.github.com>
2025-09-30 23:26:55 +0630

    Update main_jsonconnect2.js

--- main_jsonconnect2.js
+++ main_jsonconnect2.js
@@ -6,6 +6,7 @@ const CONFIG = {
     "LID-TDCJSW7R": { username: "admin", password: "1111" },
     "LID-ZFWPH5H3": { username: "admin2", password: "1221"},
      "LID-B9KJ0OKZ": { username: "admin3", password: "1234" },
+     "LID-YZMAMRTX": { username: "testuser", password:"2113"}
      "LID-W4ODZRKX": { username: "TUN", password: "1231" }
   },
   iptvUrl: "https://goldtv.adia/home"
